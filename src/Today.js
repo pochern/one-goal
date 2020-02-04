a9de65fd8c7f16542085edc6493ad1415dedfbbc
Polina Chernomaz <polina@crosscompute.com>
2020-02-03 22:58:57 -0500

    Added a dialog window for the user to enter a goal

--- src/Today.js
+++ src/Today.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles(theme => ({
   margin: {
@@ -13,13 +19,44 @@ const useStyles = makeStyles(theme => ({
 
 export default function Today() {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
 
   return (
     <div>
       <h1 className='custom-body'>Today</h1>
-      <Button variant="outlined" size="medium" color="primary" className={classes.margin}>
+      <Button variant="outlined" color="primary" className={classes.margin} onClick={handleClickOpen}>
         Add One Goal
       </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+          <DialogContentText>
+            Add your goal for today.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="goal"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
