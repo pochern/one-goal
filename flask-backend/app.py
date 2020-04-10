8953c543189c20a3f7ee4b6f0eab1d0e1edefdae
Polina Chernomaz <polina@crosscompute.com>
2020-04-09 23:05:37 -0400

    Handling PUT, POST, DELETE requests with SQLALchemy

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -8,8 +8,8 @@ db = SQLAlchemy(app)
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(80), unique=True, nullable=False)
-    # status is done in enum - restricts string to certain values
     status = db.Column(db.String(10), unique=False, nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return '<Goal %r>' % self.text
@@ -21,20 +21,40 @@ def get_data():
         'goals': [{
             'id': goal.id,
             'text': goal.text,
-            'status': goal.status,
+            'completed': goal.completed,
         } for goal in goals],
     }
 
 @app.route('/data.json', methods=['POST'])
-def set_data():
+def add_data():
     json_data = request.get_json()
     request_text = json_data['text']
-    request_status = json_data['status']
-    aGoal = Goal(text=request_text, status=request_status)
+    aGoal = Goal(text=request_text)
     db.session.add(aGoal)
     db.session.commit()
     
     return request.form
+
+@app.route('/data.json', methods=['PUT'])
+def update_data():
+    json_data = request.get_json()
+    request_text = json_data['text']
+    request_id = json_data['id']
+    request_completed = json_data['completed']
+    aGoal = Goal.query.get(request_id)
+    aGoal.text = request_text
+    aGoal.completed = request_completed
+    db.session.commit()
+
+    return request.form
+
+@app.route('/data.json/<int:id>', methods=['DELETE'])
+def delete_data(id):
+    Goal.query.filter(Goal.id == id).delete()
+    db.session.commit()
+
+    return request.form
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3100)
