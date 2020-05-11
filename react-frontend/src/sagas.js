146fd70802a7001cb56d3a86e49895777a3047ec
Polina Chernomaz <polina@crosscompute.com>
2020-05-10 21:29:22 -0400

    Fixed deleteGoal structure, created checkGoal function and a default function to yield all

--- react-frontend/src/sagas.js
+++ react-frontend/src/sagas.js
@@ -1,12 +1,10 @@
-import { put, takeLatest } from 'redux-saga/effects'
+import { put, takeLatest, all } from 'redux-saga/effects'
 
 function* fetchGoals(action) {
   try {
     // do api call
-    console.log('doing api call')
     const data = yield fetch('/data.json')
     .then(response => response.json())
-    console.log('goal data', data.goals[0].text)
     yield put({type: 'GOALS_RECEIVED', payload: data.goals})
   } catch (e) {
     console.log(e)
@@ -14,12 +12,39 @@ function* fetchGoals(action) {
 }
 
 function* deleteGoal(action) {
+  const goalId = action.payload.goalId
   try {
+    console.log('in delete')
     // do api call
-    console.log('doing api call')
-    const data = yield fetch('/data.json/' + action.id, {method: 'DELETE'}) // add type of request and json body
+    const data = yield fetch('/data.json',
+      {method: 'DELETE',
+        headers: {
+                'Content-Type': 'application/json',
+              },
+        body: JSON.stringify({'id': goalId,})
+      })
     .then(data => data.json())
     yield put({type: 'GET_GOALS'}) // after delete what do you action do you want to perform?
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function* checkGoal(action) {
+  const goal = action.payload.goal
+  const isCompleted = action.payload.isCompleted
+  try {
+    // do api call
+    const data = yield fetch('/data.json',
+      {method: 'PUT',
+        headers: {
+                  'Content-Type': 'application/json',
+                },
+        body: JSON.stringify({'id': goal.id, 'text': goal.text, 'completed': isCompleted})
+      }
+    ) // add type of request and json body
+    .then(data => data.json())
+    yield put({type: 'GOALS_CHECKED'})
   } catch (e) {
     console.log(e)
   }
@@ -31,11 +56,22 @@ function* deleteGoal(action) {
  *       dispatched while a fetch is already pending, that pending fetch is cancelled
  *         and only the latest one will be run.
  *         */
-export default function* mySaga() {
-  yield takeLatest('GET_GOALS', fetchGoals);
+export default function* () {
+  yield all([
+    getSaga(),
+    deleteSaga(),
+    checkSaga(),
+  ])
 }
 
+export function* getSaga() {
+  yield takeLatest('GET_GOALS', fetchGoals)
+}
 
 export function* deleteSaga() {
   yield takeLatest('DELETE_GOAL', deleteGoal)
+}
+
+export function* checkSaga() {
+  yield takeLatest('CHECK_GOAL', checkGoal)
 }
