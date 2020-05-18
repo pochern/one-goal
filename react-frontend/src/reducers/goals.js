661d8adb24b4800d4d4de8201418e658a0ddca11
Polina Chernomaz <polina@crosscompute.com>
2020-05-18 16:21:10 -0400

    Changed reducer names and fixed case EDIT_GOAL

--- react-frontend/src/reducers/goals.js
+++ react-frontend/src/reducers/goals.js
@@ -2,30 +2,36 @@ import produce from "immer"
 
 const goalReducer = (state = {}, action) => {
   switch(action.type) {
-    case 'GET_GOALS':
+    case 'GET_GOALS': {
       return {loading: true}
-    case 'GOALS_RECEIVED':
+    }
+    case 'GOALS_RECEIVED': {
       const { payload } = action
-      return { goals: payload, loading: false }
-    case 'DELETE_GOAL':
+      return { goalList: payload, loading: false }
+    }
+    case 'DELETE_GOAL': {
       const { goalId } = action.payload
-      return state.goals.reduce( goal => goal.id === goalId)
-    case 'CHECK_GOAL':
+      return state.goalList.reduce( goal => goal.id === goalId)
+    }
+    case 'CHECK_GOAL': {
       const { isCompleted, goal } = action.payload
       return produce(state, draft => {
-        //change goal.completed value to isCompleted
-        draft.goals.find(element => element.id===goal.id).completed = isCompleted
+        draft.goalList.find(element => element.id===goal.id).completed = isCompleted
       })
-    case 'ADD_GOAL':
+    }
+    case 'ADD_GOAL': {
       const { goalText } = action.payload
-      return produce(state, draft => {
-        draft.goals.push({
-          'text': goalText,
-          'completed': false,
-        })
-      })
-    default:
       return state
+    }
+    case 'EDIT_GOAL': {
+      const { goalText, goal } = action.payload
+      return produce(state, draft => {
+        draft.goalList.find(element => element.id===goal.id).text = goalText
+      })
+    }
+    default: {
+      return state
+    }
   }
 }
 
