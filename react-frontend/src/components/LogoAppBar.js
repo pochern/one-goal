0c11645dca9a182d0f30733af82533ba68f88526
Polina Chernomaz <polina@crosscompute.com>
2020-07-10 13:57:11 -0400

    Added appBarSignIn and appBarSignOut styles and passed props to component for button

--- react-frontend/src/components/LogoAppBar.js
+++ react-frontend/src/components/LogoAppBar.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,7 +13,14 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-  appBar: {
+  appBarSignIn: {
+    zIndex: theme.zIndex.drawer + 1,
+    borderTop: '0',
+    borderRight: '0',
+    borderLeft: '0',
+    borderBottom: '0',
+  },
+  appBarSignOut: {
     zIndex: theme.zIndex.drawer + 1,
     borderTop: '0',
     borderRight: '0',
@@ -25,19 +32,31 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function LogoAppBar() {
-  const classes = useStyles();
+export default function LogoAppBar(props) {
+  const classes = useStyles()
+  const { buttonHref, buttonVariant, buttonText } = props
 
   return (
     <div className={classes.root}>
-      <AppBar position='fixed' color='inherit' variant='outlined' className={classes.appBar}>
+      <AppBar position='fixed'
+        color='inherit'
+        variant='outlined'
+        className={buttonText === 'Sign In' ? classes.appBarSignIn : classes.appBarSignOut}
+      >
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             One Goal
           </Typography>
-          <Button variant='contained' color="secondary" className={classes.button} disableElevation>Sign out</Button>
+          <Button variant={buttonVariant}
+            color="secondary"
+            className={classes.button}
+            disableElevation
+            href={buttonHref}
+          >
+            {buttonText}
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
